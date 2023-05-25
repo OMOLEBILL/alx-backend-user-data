7454fabb2c5d9218978325b0cdd12a82ee679f69
bill omole <omolebill01@gmail.com>
2023-05-25 21:20:50 +0200

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """We reduct the messages with the given reduction
@@ -45,3 +48,18 @@ class RedactingFormatter(logging.Formatter):
         """
         return filter_datum(self.args, self.REDACTION, super().format(record),
                             self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """we implement a logger "user_data" and only log up to logging.INFO level.
+        It should not propagate messages to other loggers. It should have a
+        StreamHandler with RedactingFormatter as formatter
+    """
+    Logger = logging.getLogger("user_data")
+    Logger.setLevel(logging.INFO)
+    Logger.propagate = False
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    Logger.addHandler(handler)
+    return Logger

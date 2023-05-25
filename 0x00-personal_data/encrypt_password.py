8ae0ebca2ea21bd28c866dd31806cb35dd4aea53
bill omole <omolebill01@gmail.com>
2023-05-25 22:56:30 +0200

    validated

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,15 @@ def hash_password(password: str) -> bytes:
             a salted and hashed password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """We validate the given password
+       args:
+            hashed_password : the password to compare with
+            password: te string password
+        returns a boolen
+    """
+    if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+        return True
+    return False

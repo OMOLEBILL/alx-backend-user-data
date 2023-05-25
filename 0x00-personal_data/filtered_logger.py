1601c827c01a39a6b8676b48ad1a256b0ab4b741
bill omole <omolebill01@gmail.com>
2023-05-25 20:54:27 +0200

    format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """We implement a simple reduction"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -21,3 +22,26 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{i}=.*?{separator}',
                          f'{i}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.args = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ we implement a mini format method
+            arg:
+                record : a record from the logging.LogRecord
+            return:
+                a formatted string
+        """
+        return filter_datum(self.args, self.REDACTION, super().format(record),
+                            self.SEPARATOR)

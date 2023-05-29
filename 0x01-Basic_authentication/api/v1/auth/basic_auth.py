fd5225a6e58807cdf772f0dc58fabcbd7c98861e
bill omole <omolebill01@gmail.com>
2023-05-29 21:17:18 +0200

    documentation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ class BasicAuth(Auth):
     """This class extends methods from the auth class"""
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """We return part of the header to be encoded"""
         if authorization_header is None:
             return None
         if type(authorization_header) != str:

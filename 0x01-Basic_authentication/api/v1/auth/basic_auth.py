7036e92cb2bd9fb9e44d162765ebf98b4a6e7523
bill omole <omolebill01@gmail.com>
2023-05-29 21:07:57 +0200

    basicauth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """This class extends methods from the auth class"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        strlist = authorization_header.split(' ')
+        if len(strlist) == 1:
+            return None
+        if strlist[0] != "Basic":
+            return None
+        else:
+            return strlist[1]

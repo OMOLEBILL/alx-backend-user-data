7190f5ca5dc1bbd33d61392de5241817d60cbd9f
bill omole <omolebill01@gmail.com>
2023-05-29 20:47:17 +0200

    basic_auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This classes implement the basic authenication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """This class extends methods from the auth class"""

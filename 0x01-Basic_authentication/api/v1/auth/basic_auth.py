32095c1b814ebf5b5454d1e2e9845622cd0b071b
bill omole <omolebill01@gmail.com>
2023-05-29 21:34:26 +0200

    base64decoded

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """This classes implement the basic authenication"""
+import binascii
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """This class extends methods from the auth class"""
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """We return part of the header to be encoded"""
         if authorization_header is None:
             return None
-        if type(authorization_header) != str:
+        if not isinstance(authorization_header, str):
             return None
         strlist = authorization_header.split(' ')
         if len(strlist) == 1:
@@ -19,3 +22,15 @@ class BasicAuth(Auth):
             return None
         else:
             return strlist[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """We decode the base64 str into a utf-8"""
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            strb = base64.b64decode(base64_authorization_header)
+        except (TypeError, binascii.Error):
+            return None
+        return strb.decode('utf-8')

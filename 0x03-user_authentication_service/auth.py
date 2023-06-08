9d9a8034ed513569aef27607fa7700959fc92ac8
bill omole <omolebill01@gmail.com>
2023-06-09 00:39:49 +0200

    user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,13 @@ class Auth:
         except Exception:
             return None
         return user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """We search for a user by session_id"""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            if user.session_id:
+                return user
+            return None
+        except Exception:
+            return None

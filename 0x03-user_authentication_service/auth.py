a2be0718c96a0628aa963379ffd42c4d0ae14c8b
bill omole <omolebill01@gmail.com>
2023-06-09 01:39:45 +0200

    stilll updating the password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,16 @@ class Auth:
             raise ValueError
         except Exception:
             raise ValueError
+
+    def update_password(self, reset_token, password) -> None:
+        """We update a password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if user:
+                hashedpassword = _hash_password(password)
+                self._db.update_user(
+                    user.id, hashed_password=hashedpassword, reset_token=None)
+                return None
+            raise ValueError
+        except Exception:
+            raise ValueError
